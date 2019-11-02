fdd0f904dd78457a942b5d9f7bd06df3645d3e7c
Vincent Pang <vpang23@gmail.com>
2019-11-02 16:23:49 +0000

    fixing production issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.defaut_url_options = { host: 'quatddro-pang-tdd.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'quatddro-pang-tdd.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
